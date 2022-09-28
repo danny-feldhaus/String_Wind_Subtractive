554ca71d93f8d7f4526e9b22fbc736de9346df07
Danny Feldhaus <danny.b.feldhaus@gmail.com>
2022-09-27 17:35:51 -0700

    A new string winding algorithm, designed for transluscent string

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/danny/Documents/Prog/String_Wind_Subtractive
+# Build directory: /home/danny/Documents/Prog/String_Wind_Subtractive/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
